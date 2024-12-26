06312cfa7a098f5d4a087682ecaa50c890914092
sabeerbikba <sabeerbikba02@gmail.com>
2024-12-26 10:25:32 +0530

    retry deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import "./global.css";
+import "./index.css";
 import App from "./App";
 
 createRoot(document.getElementById("root") as HTMLDivElement).render(
